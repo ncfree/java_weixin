8790ca032c509bf94bea62013f04eabb48afc671
tom_glb <ncfree_pro@ni.com>
2020-06-03 23:04:07 +0800

    new java_weixin

---
+++ src/main/java/xyz/niyin/demo/App.java
@@ -0,0 +1,15 @@
+package xyz.niyin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
+@EnableAutoConfiguration
+public class App {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(App.class, args);
+	}
+
+}
